6a714ff7b8c353c591f73850afb6c75003d8f863
DenisSidoren <79216403243@yandex.ru>
2021-02-04 16:39:14 +0300

    add search engine

--- newsviz/search/engine.py
+++ newsviz/search/engine.py
@@ -11,7 +11,8 @@ class Ranker():
   def init(self, embs, meta):
     """
     embs: numpy array of vectors for each document
-    meta: list of arbitrary metainformation with same indexing (used for output) == sample
+    meta: list of arbitrary metainformation with same 
+    indexing (used for output) == sample
     """
     self.embs = embs
     self.meta = meta
@@ -25,15 +26,21 @@ class Ranker():
     topn: how many indexes to output
     metric: see sklearn docs for NearestNeighbors
     """
-    self.v_query = v_query
     if self.model is None:
       self.model = NearestNeighbors(metric=metric, n_jobs=-1)
       self.model.fit(self.embs)
 
-    ixs = self.model.kneighbors(self.v_query)
+    all_neighb = self.model.kneighbors([self.v_query], 2,
+                                       return_distance=False)
+    ixs = all_neighb[0]
     return ixs
-  #
+
   def get_attributes(self, ixs):
-    attrs = ixs[0]
+      """
+    Outputs metainformation for vectors 
+    which of indexes  == `ixs`(from get_nearest)
+    """
+    attrs = self.meta[ixs]
     return attrs
+
 
